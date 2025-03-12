80704740aa43536f55a2825d0d6a0fe0744b4979
Zhou Ziyi <1301210885@qq.com>
2025-03-12 10:22:01 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author ziyi zhou
  */
 public class LeapYear {
 
@@ -29,5 +29,11 @@ public class LeapYear {
             }
         }
     }
+	
+    public static boolean isLeapYear(int year) {
+	if (year % 400 == 0) return true;
+	if (year % 4 == 0 && year % 100 != 0) return true;
+	return false;
+}
 }
 
